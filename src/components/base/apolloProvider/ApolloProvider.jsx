4fbb77a9a05ae6645b91472026eea9a8115a3170
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2021-05-18 14:12:52 +0200

    changed sanity project id

--- src/components/base/apolloProvider/ApolloProvider.jsx
+++ src/components/base/apolloProvider/ApolloProvider.jsx
@@ -2,7 +2,8 @@ import { ApolloProvider as BasicApolloProvider, ApolloClient, InMemoryCache } fr
 
 
 const client = new ApolloClient({
-  uri: "https://lldcxvcf.api.sanity.io/v1/graphql/production/default",
+  // uri: "https://lldcxvcf.api.sanity.io/v1/graphql/production/default",
+  uri: "https://e24aqc8d.api.sanity.io/v1/graphql/production/default",
   cache: new InMemoryCache(),
 });
 
